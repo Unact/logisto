122520d4bbec7929f8160803d6408b3f72a6d5df
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2024-01-23 13:15:12 +0300

    DEV-42458

--- lib/app/pages/orders/order/accept_payment/accept_payment_view_model.dart
+++ lib/app/pages/orders/order/accept_payment/accept_payment_view_model.dart
@@ -69,7 +69,7 @@ class AcceptPaymentViewModel extends PageViewModel<AcceptPaymentState, AcceptPay
     ));
   }
 
-  Future<void> _getPaymentCredentials() async {
+  Future<void> _getPaymentCredentials([String? _]) async {
     if (state.canceled) return;
 
     emit(state.copyWith(message: 'Установление связи с сервером', status: AcceptPaymentStateStatus.gettingCredentials));
